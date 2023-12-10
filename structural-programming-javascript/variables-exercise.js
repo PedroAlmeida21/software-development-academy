50e89aed41f03087e0058ebbe5e396893a642ee7
Pedro Almeida <pedroalmeida199620@gmail.com>
2023-12-10 11:57:19 +0100

    feat: added variables exercise

---
+++ structural-programming-javascript/variables-exercise.js
@@ -0,0 +1,29 @@
+//1.
+//item = "test";
+//console.log(item);
+//var item;
+
+//2.
+//console.log(item);
+//item = "test";
+//var item;
+
+//3.
+console.log(item);
+let item;
+
+//4.
+console.log(item2)
+let item2 = "test";
+
+//5.
+myFunc(2, 3);
+function myFunc(a, b){
+    console.log(a + b);
+}
+
+// //6.
+var myFunc2 = function(a, b){
+    console.log(a + b);
+}
+myFunc2(2, 3);
